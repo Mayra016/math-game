9b1d81d99da75a5515cfcb52652ced56c18090e7
Mayra016 <mayrazucaro@gmail.com>
2025-08-24 19:15:11 -0300

    Feature: add components to app screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { LanguageProvider } from './components/LanguageProvider';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Menu from './pages/menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LanguageProvider>
+        <Router className="container">
+          <Routes>
+            <Route path="/menu" element={<Menu></Menu>}></Route>
+            <Route path="/" element={<Menu></Menu>}></Route>
+          </Routes>
+        </Router>
+      </LanguageProvider>
     </div>
   );
 }
