4354c849099d1bf953356577e9eeb5c9aeeba510
Mayra016 <mayrazucaro@gmail.com>
2025-08-25 20:55:38 -0300

    Feature: add an unique id for each gameMode

--- src/components/gameModes.jsx
+++ src/components/gameModes.jsx
@@ -2,11 +2,11 @@ import { useTranslation } from "../components/LanguageProvider";
 import { redirect } from "../utils/redirect";
 
 
-const GameModes = ({description, imgURL}) => {
+const GameModes = ({key, description, imgURL}) => {
     const {text} = useTranslation();
 
     return(
-        <div className="gameModes" >
+        <div className="gameModes" id={key} >
             <h3>{description}</h3>
             <img src={imgURL}></img>
             <button onClick={() => redirect('/play')}>{text("play")}</button>
