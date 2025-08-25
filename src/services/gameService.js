3681038b745f55b8b9e276136ef93b135d932421
Mayra016 <mayrazucaro@gmail.com>
2025-08-25 20:57:08 -0300

    Feature: update equation generation to meet complex equation requirements.

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -9,13 +9,13 @@ export class GameService {
     levelResult = "";
 
     updateMaxNum() {
-        if (this.level % 3 === 0) {
+        if (this.level % 6 === 0) {
             this.maxNum += 10;
         }    
     }
 
     updateMaxEquations() {
-        if (this.level % 5 === 0 && this.maxNum < 5) {
+        if (this.level % 5 === 0 && this.maxEquations < 5) {
             this.maxEquations++;
         }    
     }
@@ -33,58 +33,94 @@ export class GameService {
     }
 
     generateEquation() {
-        const randomizer = this.getRandomInt(1, this.maxNum);
-        let i = 0;
-        while (i < this.maxEquations) {
-            if (randomizer === 1) {
-                this.generateAdditionEquation();  
-            }
-            if (randomizer === 2) {
-                this.generateSubtractionEquation();
-            }
-            if (randomizer === 3) {
-                this.generateMultiplicationEquation();
-            }
-            if (randomizer === 4) {
-                this.generateDivisionEquation();
-            }
-            i++;
+        let equation = "";
+        let operations = ["+", "-", "×", "/"];
+        const numTerms = this.maxEquations + 1;
+    
+        equation += this.getRandomInt(1, this.maxNum);
+    
+        for (let i = 1; i < numTerms; i++) {
+            const op = operations[this.getRandomInt(0, operations.length - 1)];
+            const num = this.getRandomInt(1, this.maxNum);
+            equation += ` ${op} ${num}`;
         }
-
+    
+        const evalEquation = equation.replace(/×/g, "*");
+        const result = eval(evalEquation);
+    
+        this.levelEquation = equation;
+        this.levelResult = this.formatResult(result);
     }
+    
+    
 
     updateEquation(equation, result) {
+        if (this.playedEquations.includes(equation)) {
+            return false;
+        }
+
         if (this.maxEquations === 1) {
             this.levelEquation = equation;
-            this.levelResult = result;
+            this.levelResult = this.formatResult(result);
         } else {
             this.levelEquation += `+ ${equation}`;
-            this.levelResult += result;
-        }        
+            this.levelResult = this.formatResult(result);
+        }    
+        
+        this.playedEquations.push(equation);
+
+        return true;
+    }
+
+    formatResult(num){
+        if (Number.isInteger(num)) {
+            return num;
+        }
+
+        const str = num.toString();
+
+        if (str.includes(".")) {
+          const decimals = str.split(".")[1];
+          if (decimals.length > 2) {
+            return num.toFixed(2);
+          } else {
+            return num;
+          }
+        } else {
+            return num;
+        }
     }
 
     generateAdditionEquation() {
         const a = this.getRandomInt(1, this.maxNum);
         const b = this.getRandomInt(1, this.maxNum);
-        this.updateEquation(`${a} + ${b}`,a + b );
+        if (this.updateEquation(`${a} + ${b}`,a + b ) === false) {
+            this.generateEquation();
+        }
     }
 
     generateSubtractionEquation() {
         const a = this.getRandomInt(1, this.maxNum);
         const b = this.getRandomInt(1, this.maxNum);
-        this.updateEquation(`${a} - ${b}`, a - b);
+        if (this.updateEquation(`${a} - ${b}`, a - b) === false) {
+            this.generateEquation();
+        }
     }
 
     generateMultiplicationEquation() {
         const a = this.getRandomInt(1, this.maxNum);
         const b = this.getRandomInt(1, this.maxNum);
-        this.updateEquation(`${a} × ${b}`, a * b);
+        if(this.updateEquation(`${a} × ${b}`, a * b) == false) {
+            this.generateEquation();
+        }
     }
 
     generateDivisionEquation() {
         const a = this.getRandomInt(1, this.maxNum);
         const b = this.getRandomInt(1, this.maxNum);
-        this.updateEquation(`${a} / ${b}`, a / b);
+        if (this.updateEquation(`${a} / ${b}`, a / b) == false) {
+            this.generateEquation();
+        }
     }  
     
     getRandomInt(min, max) {
