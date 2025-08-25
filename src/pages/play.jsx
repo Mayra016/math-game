006ce3f8455ce9798057cc0486922f13f36317a1
Mayra016 <mayrazucaro@gmail.com>
2025-08-25 20:58:13 -0300

    Feature: add handleAnswer to update score and lives depending on user's answer

--- src/pages/play.jsx
+++ src/pages/play.jsx
@@ -1,14 +1,29 @@
 import PlayersInfo from "../components/playersInfo";
 import Game from "../components/game";
+import { useState } from "react";
 
 const Play = () =>  {
-    let lifes = 3;
-    let score = 0;
+    const [lifes, setLifes] = useState(3);
+    const [score, setScore] = useState(0);
+
+    const handleUserAnswer = (value) => {
+        if (value) {
+            setScore(prev => prev + 10);
+        } else {
+            if (lifes > 1) {
+                setLifes(prev => prev - 1);
+            } else {
+                setScore(0);
+                setLifes(3);
+            }
+            
+        }
+    }
 
     return(
         <div>
             <PlayersInfo lifes={lifes} score={score}></PlayersInfo>
-            <Game levelLifes={lifes}></Game>
+            <Game sendData={handleUserAnswer} levelLifes={lifes}></Game>
         </div>
     );
 }
