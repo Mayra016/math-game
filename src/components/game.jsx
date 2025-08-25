8a63914fcb281cb5a6de0a89701041e47acd8cf8
Mayra016 <mayrazucaro@gmail.com>
2025-08-25 20:55:02 -0300

    Feature: update value of level, maxNum and maxEquations in gameservice each level

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,18 +1,39 @@
 import { useTranslation } from "./LanguageProvider";
 import { GameService } from "../services/gameService";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
-const Game = ( {levelLifes} ) => {
+const Game = ( {levelLifes, sendData} ) => {
     const {text} = useTranslation();
     let [gameLogic, setGameLogic] = useState(new GameService());
-    const [userInput, setUserInput] = useState(null);
+    const [userInput, setUserInput] = useState("");
     const [levelEquation, setLevelEquation] = useState("");
-    
+    const gameContainer = useRef(null);
+
+    function nextLevel() {
+        gameLogic.generateEquation();
+        setLevelEquation(gameLogic.getEquation());
+        console.log(gameLogic.getLevelResult());
+    }
 
     function checkAnswer() { 
 
         if (userInput === String(gameLogic.getLevelResult())) {
             console.log("Correct");
+            gameContainer.current?.classList.add("win");
+            sendData(true);
+            setTimeout(() => gameContainer.current?.classList.remove("win"), 500);
+            setUserInput("");
+            gameLogic.setLevel();
+            gameLogic.updateMaxNum();
+            gameLogic.updateMaxEquations();
+            nextLevel();
+        } else {
+            sendData(false);
+            gameContainer.current?.classList.add("shake");
+            setTimeout(() => gameContainer.current?.classList.remove("shake"), 100);
+            if (levelLifes < 1) {
+
+            }
         }
     }
 
@@ -24,9 +45,9 @@ const Game = ( {levelLifes} ) => {
 
 
     return(		
-        <div className="game-level" >
+        <div ref={gameContainer} className="game-level" >
             <h3 id="equation" >{levelEquation}</h3>
-            <input onChange={(e) => setUserInput(e.target.value)} className="playerAnswer" type="number"></input>
+            <input value={userInput} onChange={(e) => setUserInput(e.target.value)} className="playerAnswer" type="number"></input>
             <button onClick={() => checkAnswer()}>{text('send')}</button>
         </div>
     );
