78062f997e629a076e05871fea1064f182a3f29e
Mayra016 <mayrazucaro@gmail.com>
2025-08-26 17:12:28 -0300

    Feature: adjust route to each game mode

--- src/components/gameModes.jsx
+++ src/components/gameModes.jsx
@@ -2,15 +2,16 @@ import { useTranslation } from "../components/LanguageProvider";
 import { useNavigate } from "react-router-dom";
 
 
-const GameModes = ({key, description, imgURL}) => {
+const GameModes = ({modeKey, name, description, imgURL, link}) => {
     const {text} = useTranslation();
     const navigate = useNavigate();
 
     return(
-        <div className="gameModes" id={key} >
+        <div key={modeKey} className="gameModes">
+            <h2>{name}</h2>
             <h3>{description}</h3>
             <img src={imgURL}></img>
-            <button onClick={() => navigate('/play')}>{text("play")}</button>
+            <button onClick={() => navigate(link)}>{text("play")}</button>
         </div>
     );
 };
