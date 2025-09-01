642cbc67fa52606684c4bc49c2d50eb6e1e4c759
Mayra016 <mayrazucaro@gmail.com>
2025-09-01 11:06:22 -0300

    Feature: add "enter" key press eventListener to enhance user experience

--- src/components/FreestyleMode.jsx
+++ src/components/FreestyleMode.jsx
@@ -30,6 +30,7 @@ const FreestyleMode = ( {levelLifes, sendData, levelScore} ) => {
         }).then((result) => {
             if (result.isConfirmed) {
                 gameLogic.resetLevel();
+                sendData("reset");
                 setUserInput("");
                 nextLevel();               
             } else {
@@ -75,12 +76,16 @@ const FreestyleMode = ( {levelLifes, sendData, levelScore} ) => {
         setLevelEquation(gameLogic.getEquation());
     }, []);
       
-
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            checkAnswer();
+        }
+    };
 
     return(		
         <div ref={gameContainer} className="game-level" >
             <h3 id="equation" >{levelEquation}</h3>
-            <input value={userInput} onChange={(e) => setUserInput(e.target.value)} className="playerAnswer" type="number"></input>
+            <input value={userInput} onChange={(e) => setUserInput(e.target.value)} onKeyPress={handleKeyPress} className="playerAnswer" type="number"></input>
             <button onClick={() => checkAnswer()}>{text('send')}</button>
             <audio ref={audioEffects} src=""></audio>
         </div>
