55af47e57b416f745d7d463f7bc0c9f1dafc840c
Mayra016 <mayrazucaro@gmail.com>
2025-09-01 09:33:35 -0300

    Feature: add One Minute game mode to menu

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -5,13 +5,17 @@ import music from '../assets/music.mp3';
 import GameModesInfo from '../models/gameModesInfo';
 import freeStyleImg from '../assets/freestyle-icon.png';
 import timmerIcon from '../assets/timmer-icon.png';
+import oneMinuteIcon from '../assets/one-minute-icon.png';
+import greaterThanIcon from '../assets/greaterThanIcon.png';
 
 
 const Menu = () => {
     const {text} = useTranslation();
     const volume = localStorage.getItem("vol") || 50;
     const audioElement = useRef(null);
-    const gameModes = [new GameModesInfo("Freestyle", text("equations"), freeStyleImg), new GameModesInfo("Beat The Clock", text("clock-description"), timmerIcon)];
+    const gameModes = [new GameModesInfo("Freestyle", text("equations"), freeStyleImg), new GameModesInfo("Beat The Clock", text("clock-description"), timmerIcon),
+                       new GameModesInfo("One Minute", text("one-description"), oneMinuteIcon)
+    ];
 
     useEffect(() => {
         if (audioElement.current) {
